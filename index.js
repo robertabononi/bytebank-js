8494a8c607dc8737889582d994f8396a39325421
Roberta Bononi da Silva <roberta.bononi16@gmail.com>
2021-09-06 23:15:08 -0300

    chore: create class 'Cliente' and two class' instance

---
+++ index.js
@@ -0,0 +1,19 @@
+class Cliente {
+    nome;
+    cpf;
+    agencia;
+    saldo;
+}
+
+const cliente1 = new Cliente();
+const cliente2 = new Cliente();
+
+cliente1.nome = "Ricardo";
+cliente1.cpf = 11122233309;
+cliente1.agencia = 1001;
+cliente1.saldo = 0;
+
+cliente2.nome = "Alice";
+cliente2.cpf = 77766655505;
+cliente2.agencia = 1001;
+cliente2.saldo = 100;
