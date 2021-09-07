47759159e6d7e46cd621c7dc730bd700637feacf
Roberta Bononi da Silva <roberta.bononi16@gmail.com>
2021-09-06 23:51:54 -0300

    feat: create class 'ContaCorrente' and method 'sacar'

--- index.js
+++ index.js
@@ -1,19 +1,32 @@
 class Cliente {
     nome;
     cpf;
+}
+
+class ContaCorrente {
     agencia;
     saldo;
+
+    sacar(valor) {
+        if(this.saldo >= valor) {
+            this.saldo -= valor;
+        }
+    }
 }
 
 const cliente1 = new Cliente();
-const cliente2 = new Cliente();
-
 cliente1.nome = "Ricardo";
 cliente1.cpf = 11122233309;
-cliente1.agencia = 1001;
-cliente1.saldo = 0;
 
+const cliente2 = new Cliente();
 cliente2.nome = "Alice";
 cliente2.cpf = 77766655505;
-cliente2.agencia = 1001;
-cliente2.saldo = 100;
+
+const contaCorrenteRicardo = new ContaCorrente();
+contaCorrenteRicardo.saldo = 0;
+contaCorrenteRicardo.agencia = 1001;
+contaCorrenteRicardo.sacar(50);
+console.log(contaCorrenteRicardo.saldo);
+
+console.log(cliente1);
+console.log(cliente2);
