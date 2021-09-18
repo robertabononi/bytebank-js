3fffc3f189bc0d52c97eccf5940bb7ca3744182c
Roberta Bononi da Silva <roberta.bononi16@gmail.com>
2021-09-17 21:05:14 -0300

    chore: use value as a variable

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ const cliente1 = new Cliente();
 cliente1.nome = "Ricardo";
 cliente1.cpf = 11122233309;
 
-const cliente2 = new Cliente();
-cliente2.nome = "Alice";
-cliente2.cpf = 77766655505;
+//const cliente2 = new Cliente();
+//cliente2.nome = "Alice";
+//cliente2.cpf = 77766655505;
 
 const contaCorrenteRicardo = new ContaCorrente();
 contaCorrenteRicardo.agencia = 1001;
@@ -16,8 +16,11 @@ contaCorrenteRicardo.depositar(500);
 
 const conta2 = new ContaCorrente();
 conta2.agencia = 102;
-conta2.cliente = cliente2;
+conta2.cliente = new Cliente();
+conta2.cliente.nome = "Alice";
+conta2.cliente.cpf = 77766655505;
 
-contaCorrenteRicardo.transferir(200,conta2);
+let valor = 200;
+contaCorrenteRicardo.transferir(valor, conta2);
 
 console.log(conta2);
